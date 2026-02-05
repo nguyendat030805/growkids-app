fadfedbddf8c711188913a9a470fdbcce6fb6ffc
To Nga <ngaichi2004@gmail.com>
2026-02-06 01:24:51 +0700

    feat(GK-39)/Add-experiences-homepage

---
+++ src/features/experiences/routes/ExperienceRoute.tsx
@@ -0,0 +1,5 @@
+import ExperienceScreen from "@/src/features/experiences/pages/ExperienceScreen";
+
+export default function ExperienceRoute() {
+  return <ExperienceScreen />;
+}
--- src/routes/AppNavigator.tsx
+++ src/routes/AppNavigator.tsx
@@ -1,4 +1,6 @@
 import HomepageRoute from "@/src/features/homepage/routes/HomepageRoute";
+import ExperienceRoute from "@/src/features/experiences/routes/ExperienceRoute";
 export default function AppNavigator() {
-  return <HomepageRoute />;
+  // return <HomepageRoute />;
+  return <ExperienceRoute />;
 }
