9bb8c14f41bb9eb4a0229e43ea1a7194d294426d
Nguyen Van Dat <dat656473@gmail.com>
2026-02-23 21:23:22 +0700

    fix(shedule): resolve schedule issue

--- src/routes/AppNavigator.tsx
+++ src/routes/AppNavigator.tsx
@@ -3,6 +3,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { RootStackParamList } from "../core/navigation/NavigationService";
 import NotFoundScreen from "../core/pages/PageNotFound";
 import LoginScreen from "../features/auth/pages/LoginScreen";
+import HomepageScreen from "../features/homepage/pages/HomepageScreen";
 import OnboardingPage from "../features/schedules/pages/OnboardingPage";
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -16,7 +17,7 @@ export default function AppNavigator() {
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Onboarding" component={OnboardingPage} />
       <Stack.Screen name="NotFound" component={NotFoundScreen} />
-      <Stack.Screen name="MainHome" component={NotFoundScreen} />
+      <Stack.Screen name="MainHome" component={HomepageScreen} />
       <Stack.Screen name="GoldenTime" component={NotFoundScreen} />
     </Stack.Navigator>
   );
